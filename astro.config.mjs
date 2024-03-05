bdefbf92ba160e46a7d42cbb94d016db940c9bd6
Harith Onigemo <onigemotosin@gmail.com>
2024-03-05 20:48:29 +0400

    :zap: added vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   include: ["src/components/**/*.astro.jsx"],
-  integrations: [preact()]
+  integrations: [preact()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
